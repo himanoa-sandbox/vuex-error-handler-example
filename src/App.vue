d0a98ac0552663fec5763e5b870d1e79b00ba545
himanoa <matsunoappy@gmail.com>
2018-12-18 03:01:45 +0900

    Implement

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="navbar">
+      <p class="title">example!</p>
+    </div>
+    <div class="box">
+      <a class="button is-primary" @click="ok">押すとOKが出るボタン</a>
+      <a class="button is-danger" @click="onClickFail">押すと例外が飛ぶボタン</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapActions } from "vuex";
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  methods: {
+    ...mapActions("example", ["ok", "fail"]),
+    onClickFail(e) {
+      this.fail().catch(err => {
+        this.$toasted.show(err.message, {
+          position: "top-center"
+        })
+      })
+    }
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: inherit;
+}
+html, body {
+  height: 100%;
+  margin: 0;
+}
+div {
+  display: block;
+}
+.box {
+  padding: 1em;
+  margin: 1em;
+}
+.button {
+  border: 1px solid;
+  padding: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  display: inline-block;
+  cursor: pointer;
+}
+.is-primary {
+  background-color: #0074bf;
+  color: #fff;
+}
+.is-danger {
+  background-color: #c60019;
+  color: #fff;
+}
+.navbar {
+  flex: 1;
+  background-color: #d16b16;
+  padding-left: 0.5rem;
+  font-weight: bold;
+  color: #fff;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import Toasted from "vue-toasted"
 
 Vue.config.productionTip = false
+Vue.use(Toasted)
+Vue.use(Vuex)
+
+const example = {
+  namespaced: true,
+  actions: {
+    async ok(context) {
+      console.log("ok")
+    },
+    async fail(context) {
+      throw new Error("エラー");
+    }
+  }
+}
+const store = new Vuex.Store({
+  modules: {
+    example
+  }
+})
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
